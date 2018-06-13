a5570d32ac1af8e255de31d45107ba0ded9cc29f
ThePuffin <ballutvincent@gmail.com>
2018-06-13 09:03:03 +0200

    ajout de reactstrap et de bootstrap

--- checkpoint3/src/components/App.js
+++ checkpoint3/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Checkpoint from './Checkpoint';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>Checkpoint3</h1>
-       
+       <Checkpoint/>
       </div>
     );
   }
